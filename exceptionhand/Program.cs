5c8bba38102eb661a0ce82ab12f0d0af5fd1f371
abdelrahmanamr54 <amrmmi22@gmail.com>
2023-12-20 23:15:25 +0200

    first commit

---
+++ exceptionhand/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace exceptionhand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            try
+            {
+                List<int> numbers = new List<int> { 10,20};
+                CheckForDuplicates(numbers);
+                Console.WriteLine("No duplicates found. All numbers are unique.");
+            }
+            catch(excptionhand ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"good job");
+
+            }
+
+
+
+
+        }
+        static void CheckForDuplicates(List<int> numbers)
+        {
+            Console.WriteLine("enter yr fav number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            foreach (int number in numbers)
+            {
+                if (number == num)
+                {
+                    throw new excptionhand(number);
+                }
+                else
+                {
+                    numbers.Add(num);
+                    Console.WriteLine("done");
+                }
+            }
+
+        }
+    }
+}
---
+++ exceptionhand/excptionhand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionhand
+{
+    internal class excptionhand: Exception
+    {
+        public int DuplicateNumber { get; }
+        //public exceptionhand(int duplicateNumber)
+        //{
+        //   
+
+        //}
+        public excptionhand(int duplicateNumber) : base($"Duplicate number found: {duplicateNumber}")
+        {
+            DuplicateNumber = duplicateNumber;
+        }
+    }
+}
